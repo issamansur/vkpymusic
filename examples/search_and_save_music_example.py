b1d27ea5274167a10eb8cc9cc581326d1b88113d
User#None <77580844+issamansur@users.noreply.github.com>
2023-11-07 00:42:19 +0200

    Add `else` statement in example

--- examples/search_and_save_music_example.py
+++ examples/search_and_save_music_example.py
@@ -9,6 +9,8 @@ if service is not None:
     songs = service.search_songs_by_text(input())
     if songs:
         service.save_music(songs[0])
+    else:
+        print("No songs found ._.")
 else:
     print("File config not found!")
     print("Run first 'examples\\receive_token_example.py'")
