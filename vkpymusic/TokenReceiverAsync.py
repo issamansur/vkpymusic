34ac82a25b7d2301fa3b0c453a2a9649d32317c8
Romanin <60302782+romanin-rf@users.noreply.github.com>
2023-12-15 08:51:19 +0200

    (Hotfix) AttributeError: 'coroutine' object has no attribute 'content'

--- vkpymusic/TokenReceiverAsync.py
+++ vkpymusic/TokenReceiverAsync.py
@@ -126,7 +126,7 @@ class TokenReceiverAsync:
                 logger.warn("Invalid code. Try again!")
                 code: str = on_2fa()
 
-                response_auth = self.__request_auth(code=code)
+                response_auth = await self.__request_auth(code=code)
                 response_auth_json = json.loads(response_auth.content.decode("utf-8"))
 
             elif error == "invalid_client":
