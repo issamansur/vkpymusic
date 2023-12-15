b8adcf76e2252e6d0c454d513d4f82546a4d5ea0
Romanin <60302782+romanin-rf@users.noreply.github.com>
2023-12-15 23:38:02 +0200

    Update TokenReceiverAsync.py

--- vkpymusic/TokenReceiverAsync.py
+++ vkpymusic/TokenReceiverAsync.py
@@ -124,7 +124,7 @@ class TokenReceiverAsync:
                 
             elif error == "invalid_request":
                 logger.warn("Invalid code. Try again!")
-                code: str = on_2fa()
+                code: str = await on_2fa()
 
                 response_auth = await self.__request_auth(code=code)
                 response_auth_json = json.loads(response_auth.content.decode("utf-8"))
