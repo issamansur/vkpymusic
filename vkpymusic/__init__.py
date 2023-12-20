8ddf85e8c8b7383dbb7eef59ad07170f532aa1f0
User#None <issa_mansur@mail.ru>
2023-12-20 22:40:49 +0200

    work with `docstring` and `sphinx` - 1

--- vkpymusic/__init__.py
+++ vkpymusic/__init__.py
@@ -1,3 +1,25 @@
+"""
+VKpuMusic
+~~~~~~~~~
+
+A library for interacting with music in VK.
+
+Provides a convenient API for authorization, 
+receiving audio recordings, searching for music,
+as well as downloading individual songs and 
+playlists of the user in VK.
+
+:copyright: (c) 2023-present issamansur (EDEXADE, inc)
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = "vkpymusic"
+__author__ = "issamansur"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023-present issamansur (EDEXADE, inc)"
+__version__ = "3.0.1"
+
 from .client import Client, clients
 from .token_receiver import TokenReceiver
 from .token_receiver_async import TokenReceiverAsync
@@ -9,13 +31,13 @@ from .playlist import Playlist
 from . import utils
 
 __all__ = [
-    'Client',
-    'clients',
-    'TokenReceiver',
-    'TokenReceiverAsync',
-    'Service',
-    'ServiceAsync',
-    'Song',
-    'Playlist',
-    'utils',
+    "Client",
+    "clients",
+    "TokenReceiver",
+    "TokenReceiverAsync",
+    "Service",
+    "ServiceAsync",
+    "Song",
+    "Playlist",
+    "utils",
 ]
--- vkpymusic/utils/converter.py
+++ vkpymusic/utils/converter.py
@@ -1,3 +1,22 @@
+"""
+The `converter` module provides the `Converter` class used 
+for converting responses from VK into lists of songs or playlists.
+
+Classes:
+    Converter: A class that converts a response from VK into a list of songs or playlists.
+
+Functions:
+    response_to_songs(response: Response) -> List[Song]: Converts a response into a list of songs.
+    response_to_playlists(response: Response) -> List[Playlist]: Converts a response into a list of playlists.
+
+Exceptions:
+    - Describe any exceptions that can be raised in this module here.
+
+Usage Examples:
+    - Describe examples of how to use this module here.
+"""
+
+
 import json
 import logging
 from typing import List
@@ -11,9 +30,25 @@ from .logger import get_logger
 
 logger: logging.Logger = get_logger(__name__)
 
+
 class Converter:
+    """A class that converts a response from VK to a list of songs or playlists.
+
+    Methods:
+        response_to_songs(response: Response) -> List[Song]: Converts a response to a list of songs.
+        response_to_playlists(response: Response) -> List[Playlist]: Converts a response to a list of playlists.
+    """
+
     @staticmethod
     def response_to_songs(response: Response):
+        """Converts a response to a list of songs.
+
+        Args:
+            response (Response): The response object from VK.
+
+        Returns:
+            List[Song]: A list of songs converted from the response.
+        """
         response = json.loads(response.content.decode("utf-8"))
         try:
             items = response["response"]["items"]
@@ -27,6 +62,14 @@ class Converter:
 
     @staticmethod
     def response_to_playlists(response: Response):
+        """Converts a response to a list of playlists.
+
+        Args:
+            response (Response): The response object from VK.
+
+        Returns:
+            List[Playlist]: A list of playlists converted from the response.
+        """
         response = json.loads(response.content.decode("utf-8"))
         try:
             items = response["response"]["items"]
