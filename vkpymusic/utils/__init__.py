8dd1cca8db4ae5163f7eedc6d5043cf38c3d68d6
User#None <issa_mansur@mail.ru>
2023-12-21 02:30:07 +0200

    work with `docstring` and `sphinx` - 2

--- vkpymusic/utils/__init__.py
+++ vkpymusic/utils/__init__.py
@@ -1,7 +1,16 @@
+"""
+This module contains utilities for conversion and logging.
+
+Classes:
+    - Converter: A class for performing various conversion operations.
+    - get_logger: A function for getting or creating a logger.
+
+"""
+
 from .converter import Converter
 from .logger import get_logger
 
 __all__ = [
-    'Converter',
-    'get_logger',
+    "Converter",
+    "get_logger",
 ]
--- vkpymusic/utils/logger.py
+++ vkpymusic/utils/logger.py
@@ -1,8 +1,24 @@
+"""
+This module provides a logger utility for vkpymusic.
+
+It defines a logger class and functions to configure and retrieve loggers.
+
+Classes:
+- BCOLORS: A class that defines color codes for log messages.
+
+Functions:
+- _get_file_handler: Returns a file handler for logging to a file.
+- _get_stream_handler: Returns a stream handler for logging to the console.
+- get_logger: Returns a logger instance with configured handlers.
+
+"""
+
 import os
 import logging
 import datetime
 
 
+# Color codes for log messages
 class BCOLORS:
     CRITICAL = "\033[95m"
     OKBLUE = "\033[94m"
@@ -12,10 +28,19 @@ class BCOLORS:
     ERROR = "\033[91m"
     ENDC = "\033[0m"
 
-_log_format = "%(asctime)s | [%(name)s | (%(filename)s) .%(funcName)s(%(lineno)d)] [%(levelname)s] %(message)s"
+
+_log_format = "%(asctime)s | %(filename)s(%(lineno)d) | %(module)s.%(funcName)s(...) | [%(levelname)s] %(message)s"
 
 
-def get_file_handler():
+def _get_file_handler():
+    """
+    Returns a file handler for logging to a file.
+
+    The log file is created in the 'logs' directory with the name 'vkpymusic_<current_date>.log'.
+
+    Returns:
+    - file_handler: A file handler instance for logging to a file.
+    """
     file_path = f"logs/vkpymusic_{datetime.date.today()}.log"
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     file_handler = logging.FileHandler(file_path)
@@ -24,7 +49,13 @@ def get_file_handler():
     return file_handler
 
 
-def get_stream_handler():
+def _get_stream_handler():
+    """
+    Returns a stream handler for logging to the console.
+
+    Returns:
+    - stream_handler: A stream handler instance for logging to the console.
+    """
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(logging.Formatter(_log_format))
@@ -32,8 +63,17 @@ def get_stream_handler():
 
 
 def get_logger(name):
+    """
+    Returns a logger instance with configured handlers.
+
+    Args:
+    - name: The name of the logger.
+
+    Returns:
+    - logger: A logger instance with configured handlers.
+    """
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    logger.addHandler(get_file_handler())
-    logger.addHandler(get_stream_handler())
+    logger.addHandler(_get_file_handler())
+    logger.addHandler(_get_stream_handler())
     return logger
