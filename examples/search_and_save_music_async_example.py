a92e86dfd10428d90b913522309b24f9ae5144c8
User#None <77580844+issamansur@users.noreply.github.com>
2025-04-14 08:28:43 +0300

    Update search_and_save_music_async_example.py

--- examples/search_and_save_music_async_example.py
+++ examples/search_and_save_music_async_example.py
@@ -1,22 +1,22 @@
-from vkpymusic import ServiceAsync
+from vkpymusic import Service
 
 import asyncio
 
 
 async def main():
-    service = ServiceAsync.parse_config()
+    service = Service.parse_config()
     # or you can do like this
     # service = Service("<your_user_agent>", "<your_token>")
 
     if service is not None:
-        if not await service.check_token():
+        if not await service.check_token_async():
             print("Token expired!")
             print("Run first 'receive_token_example.py'")
             return
 
-        songs = await service.search_songs_by_text(input())
+        songs = await service.search_songs_by_text_async(input())
         if songs:
-            await service.save_music(songs[0])
+            await service.save_music_async(songs[0])
         else:
             print("No songs found ._.")
     else:
